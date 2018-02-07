3ded139ed2d260ebcc4fb7cd3c27b53caf3c2eaa
s147682 <tmrgast@live.nl>
2018-02-07 14:04:32 +0100

    changes

--- list/models.py
+++ list/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     avatar = models.ImageField(upload_to = 'profiel/', default = 'media/profile.png')
     naam = models.CharField(max_length = 100)
--- list/views.py
+++ list/views.py
@@ -5,16 +5,23 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def index(request):
-    posts = Post.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
+    posts = Post.objects.all()
+
     if request.method == "POST":
         form = PostForm(request.POST)
+
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.completed_date = timezone.now()
             post.save()
-            return redirect('index', {'form': form}, {'posts': posts} )
+            return redirect('index', form)
     else:
         form = PostForm()
-    return render(request, 'list/index.html', {'form': form}, {'posts': posts})
+
+    data = {
+        'posts': posts,
+        'form': form
+    }
+    return render(request, 'list/index.html', data)
     #return render(request, 'list/index.html', {'posts': posts})
